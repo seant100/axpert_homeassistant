45763b5ba91efbada71a61ea771e5e5ec0abd541
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-06-17 14:09:13 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+CURRENTUSERNAME=pi
+
+echo "Sudo User: $CURRENTUSERNAME"
+
+SUDO=
+if [ "$UID" != "0" ]; then
+	if [ -e /usr/bin/sudo -o -e /bin/sudo ]; then
+		SUDO=sudo
+	else
+		echo '*** This quick installer script requires root privileges.'
+		exit 0
+	fi
+fi
+
+$SUDO apt update
+$SUDO apt install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common \
+     git
+
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | $SUDO apt-key add -
+
+echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    $SUDO tee /etc/apt/sources.list.d/docker.list
+
+$SUDO apt update
+
+$SUDO apt install -y --no-install-recommends \
+    docker-ce \
+    cgroupfs-mount
+
+$SUDO systemctl enable docker
+$SUDO systemctl start docker
+
+$SUDO apt install -y python3-pip libffi-dev
+
+$SUDO pip3 install docker-compose
+
+$SUDO usermod -a -G docker pi
+$SUDO exec sg docker newgrp `id -gn`
+
+cd /home/pi
+git clone https://github.com/BionicWeb/AxpertPi.git
+cd /home/pi/AxpertPi
+
+$SUDO mkdir /home/pi/homeassistant
+$SUDO cp -avr www /home/pi/homeassistant
+$SUDO cp -avr homeassistant /home/pi/
+
+cd /home/pi/homeassistant
+tar -xzvf storage.tar.gz
+$SUDO rm -rf /home/pi/AxpertPi
+$SUDO rm /home/pi/homeassistant/.storage/core.restore_state
+
+$SUDO git clone https://github.com/BionicWeb/docker-voltronic-homeassistant.git /opt/ha-inverter-mqtt-agent
+cd /opt/ha-inverter-mqtt-agent
+
+docker-compose up -d
---
+++ sources/inverter-mqtt/mqtt-init.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Simple script to register the MQTT topics when the container starts for the first time...
+
+MQTT_SERVER=`cat /etc/inverter/mqtt.json | jq '.server' -r`
+MQTT_PORT=`cat /etc/inverter/mqtt.json | jq '.port' -r`
+MQTT_TOPIC=`cat /etc/inverter/mqtt.json | jq '.topic' -r`
+MQTT_DEVICENAME=`cat /etc/inverter/mqtt.json | jq '.devicename' -r`
+MQTT_USERNAME=`cat /etc/inverter/mqtt.json | jq '.username' -r`
+MQTT_PASSWORD=`cat /etc/inverter/mqtt.json | jq '.password' -r`
+
+registerTopic () {
+    mosquitto_pub \
+        -h $MQTT_SERVER \
+        -p $MQTT_PORT \
+        -u "$MQTT_USERNAME" \
+        -P "$MQTT_PASSWORD" \
+        -t "$MQTT_TOPIC/sensor/"$MQTT_DEVICENAME"_$1/config" \
+        -m "{
+            \"name\": \""$MQTT_DEVICENAME"_$1\",
+            \"unit_of_measurement\": \"$2\",
+            \"state_topic\": \"$MQTT_TOPIC/sensor/"$MQTT_DEVICENAME"_$1\",
+            \"icon\": \"mdi:$3\"
+        }"
+}
+
+registerInverterRawCMD () {
+    mosquitto_pub \
+        -h $MQTT_SERVER \
+        -p $MQTT_PORT \
+        -u "$MQTT_USERNAME" \
+        -P "$MQTT_PASSWORD" \
+        -t "$MQTT_TOPIC/sensor/$MQTT_DEVICENAME/config" \
+        -m "{
+            \"name\": \""$MQTT_DEVICENAME"\",
+            \"state_topic\": \"$MQTT_TOPIC/sensor/$MQTT_DEVICENAME\"
+        }"
+}
+
+registerTopic "Inverter_mode" "" "solar-power" # 1 = Power_On, 2 = Standby, 3 = Line, 4 = Battery, 5 = Fault, 6 = Power_Saving, 7 = Unknown
+registerTopic "AC_grid_voltage" "V" "power-plug"
+registerTopic "AC_grid_frequency" "Hz" "current-ac"
+registerTopic "AC_out_voltage" "V" "power-plug"
+registerTopic "AC_out_frequency" "Hz" "current-ac"
+registerTopic "PV_in_voltage" "V" "solar-panel-large"
+registerTopic "PV_in_current" "A" "solar-panel-large"
+registerTopic "PV_in_watts" "W" "solar-panel-large"
+registerTopic "PV_in_watthour" "Wh" "solar-panel-large"
+registerTopic "SCC_voltage" "V" "current-dc"
+registerTopic "Load_pct" "%" "brightness-percent"
+registerTopic "Load_watt" "W" "chart-bell-curve"
+registerTopic "Load_watthour" "Wh" "chart-bell-curve"
+registerTopic "Load_va" "VA" "chart-bell-curve"
+registerTopic "Bus_voltage" "V" "details"
+registerTopic "Heatsink_temperature" "" "details"
+registerTopic "Battery_capacity" "%" "battery-outline"
+registerTopic "Battery_voltage" "V" "battery-outline"
+registerTopic "Battery_charge_current" "A" "current-dc"
+registerTopic "Battery_discharge_current" "A" "current-dc"
+registerTopic "Load_status_on" "" "power"
+registerTopic "SCC_charge_on" "" "power"
+registerTopic "AC_charge_on" "" "power"
+registerTopic "Battery_recharge_voltage" "V" "current-dc"
+registerTopic "Battery_under_voltage" "V" "current-dc"
+registerTopic "Battery_bulk_voltage" "V" "current-dc"
+registerTopic "Battery_float_voltage" "V" "current-dc"
+registerTopic "Max_grid_charge_current" "A" "current-ac"
+registerTopic "Max_charge_current" "A" "current-ac"
+registerTopic "Out_source_priority" "" "grid"
+registerTopic "Charger_source_priority" "" "solar-power"
+registerTopic "Battery_redischarge_voltage" "V" "battery-negative"
+
+# Add in a separate topic so we can send raw commands from assistant back to the inverter via MQTT (such as changing power modes etc)...
+registerInverterRawCMD
