a1dcb2e90908813ad12a3047d71830694375a0fa
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-06-17 14:40:52 +0200

    Initial commit

--- install.sh
+++ install.sh
@@ -54,10 +54,8 @@ $SUDO cp -avr homeassistant /home/pi/
 
 cd /home/pi/homeassistant
 tar -xzvf storage.tar.gz
-$SUDO rm -rf /home/pi/AxpertPi
 $SUDO rm /home/pi/homeassistant/.storage/core.restore_state
 
-$SUDO git clone https://github.com/BionicWeb/docker-voltronic-homeassistant.git /opt/ha-inverter-mqtt-agent
-cd /opt/ha-inverter-mqtt-agent
+cd /home/pi/axpert_homeassistant
 
 docker-compose up -d
